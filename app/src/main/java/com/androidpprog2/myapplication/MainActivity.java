c0b802633fc52b5ed60b55e73bbcb0e456ddd7bd
RicardVG <ricardvinolas10@gmail.com>
2022-02-18 13:19:25 +0100

    Commit Visualització Layout i connexió al Mobil a través de USB + Creació de Strings

--- app/src/main/java/com/androidpprog2/myapplication/MainActivity.java
+++ app/src/main/java/com/androidpprog2/myapplication/MainActivity.java
@@ -3,12 +3,22 @@ package com.androidpprog2.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mTrueButton;
+    private Button mFalseButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+      //  mTrueButton.setOnClickListener(new View.OnClickListener());
+        mFalseButton = (Button) findViewById(R.id.false_button);
+
+
     }
 }
